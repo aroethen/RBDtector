f7c6242424fc3a4f3937adcadd07283cfe5a41c3
Annika Roethenbacher <a.roethenbacher@fz-juelich.de>
2020-09-09 16:41:48 +0200

    Add first implementation of tkinter-GUI (still in main() - yet to be extracted).

---
+++ RBDtector/gui/main_window.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+
+def start_main_window():
+    root = tk.Tk()
+    root.title('RBDtector')
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    pass
